6c8474f3b7f89f6c0ce8aa37c3a6a571f1fdfb48
lizaedwar <edwar2016pi21la@gmail.com>
2024-01-16 16:55:46 -0600

    seccion de noticias

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -5,9 +5,14 @@ use CodeIgniter\Router\RouteCollection;
 /**
  * @var RouteCollection $routes
  */
+
+use App\Controllers\News;
 use App\Controllers\Pages;
 
 $routes->get('/', 'Home::index');
+
+$routes->get('news', [News::class, 'index']);
+$routes->get('news/(:segment)', [News::class, 'show']);
 
 $routes->get('pages', [Pages::class, 'index']);
 $routes->get('(:segment)', [Pages::class, 'view']);
